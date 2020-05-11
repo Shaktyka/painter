444bd6305f2a28cf34733b3946c0e6e22851c50c
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-05-11 20:24:43 +0500

    Добавляет кнопку для очистки всего холста

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const canvas = document.querySelector('.canvas');
 const chosenColor = document.querySelector('.chosen-color');
 const eraser = document.querySelector('.eraser');
+const clearBtn = document.querySelector('.clear');
 
 canvas.addEventListener('click', function(evt) {
   let clicked = evt.target;
@@ -10,3 +11,10 @@ canvas.addEventListener('click', function(evt) {
     clicked.style.backgroundColor = 'white';
   }
 });
+
+clearBtn.onclick = () => {
+  const pixels = document.querySelectorAll('.pixel');
+  pixels.forEach((pixel) => {
+  	pixel.style.backgroundColor = 'white';
+  });
+};
